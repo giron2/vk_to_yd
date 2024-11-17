fd38b038668f2bbace0d8cd9c5338cca17e07135
Giron <srwntxpa@gmail.com>
2024-11-17 15:25:37 +0500

    commit_2

--- main.py
+++ main.py
@@ -2,20 +2,24 @@ import requests
 import json
 import yadisk
 from datetime import datetime
-
+from tqdm import tqdm
 
 
 
 
 
 class vk():
-	def upload_ile(i):
-		access_token = '6529184065291840652918402d660851486652965291840023d294536869b369cdf463e'
-		api_yandex = ()
-		with open("api_yandex.txt") as file:
+	def __init__(self, i, yd_api):
+		self.i = i
+		self.yd_api = yd_api
+
+	def upload_ile(self):
+		access_token = ()
+		with open("api_vk.txt") as file:
 			api = file.read()
-			api_yandex = api
-		vk_id = i
+			access_token = api
+		api_yandex = self.yd_api
+		vk_id = self.i
 		param_vk = {'access_token': access_token,
 			         'owner_id': vk_id,
 			         'album_id': 'profile',
@@ -32,7 +36,7 @@ class vk():
 		items = all_respons['items']
 		yd.mkdir(vk_id)
 		info_file = []
-		for item in items:
+		for item in tqdm(items):
 			for size in item["sizes"]:
 				info = dict()
 				if size['type'] == 'z':
@@ -43,21 +47,19 @@ class vk():
 					                    headers=headers)
 
 					if path.status_code != 200:
-
-
 						p = yd.upload_url(f'{size['url']}', f'/{vk_id}/{item['likes']['count']}.jpg')
-						if p.get_status() == 'success':
-							print(f'Файл {item['likes']['count']}.jpg  успешно загружен на ЯндексДиск')
 					elif path.status_code == 200 :
 						date = datetime.fromtimestamp(item['date'])
 						p = yd.upload_url(f'{size['url']}', f'/{vk_id}/{date}-'
 						                                    f'{item['likes']['count']}.jpg')
-						if p.get_status() == 'success':
-							print(f'Файл {date}-{item['likes']['count']}.jpg  успешно загружен на ЯндексДиск')
+
 
 
 		with open("info_file.json", "w") as f:
 		    f.write(str(info_file))
 
-d = vk
-d.upload_ile('618713577')
+
+
+
+d = vk('6*****', 'y0_A******')
+d.upload_ile()
